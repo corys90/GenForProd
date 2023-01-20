6acacffde63710554ab8c9ee718772198a551c27
Cristian A. Cortes Sarmiento <corys90@hotmail.com>
2023-01-20 07:47:15 -0500

    Update: Pantallas Finalizadas y funcionales

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,73 @@
 import  'bootstrap/dist/css/bootstrap.min.css' ;
 import Home from './pages/Inicio';
-//import PageConsumo from './pages/Consumo';
-//import PageProduccion from './pages/Produccion';
-//import PageConsprod from './pages/Consprod';
+import PersonalizableTabla from '../src/components/PersonalizableTabla';
+import { Container } from 'react-bootstrap';
+
+
+const dataUltimosMov: string[][] = [
+  ["Forestal Leonera", "24-10-2023", "$50.000", "En Proceso","Dental", "24-10-2023", "$10.000", "En Proceso"],
+  ["Forestal Leonera", "24-10-2023", "$50.000", "En Proceso","Dental", "24-10-2023", "$20.000", "En Proceso"],
+  ["Forestal Leonera", "24-10-2023", "$50.000", "En Proceso","Dental", "24-10-2023", "$30.000", "En Proceso"],
+  ["Forestal Leonera", "24-10-2023", "$50.000", "En Proceso","Dental", "24-10-2023", "$40.000", "En Proceso"],
+  ["Forestal Leonera", "24-10-2023", "$50.000", "En Proceso","Dental", "24-10-2023", "$50.000", "En Proceso"],
+  ["Forestal Leonera", "24-10-2023", "$50.000", "En Proceso","Dental", "24-10-2023", "$60.000", "En Proceso"],
+  ["Forestal Leonera", "24-10-2023", "$50.000", "En Proceso","Dental", "24-10-2023", "$70.000", "En Proceso"],
+  ["Forestal Leonera", "24-10-2023", "$50.000", "En Proceso","Dental", "24-10-2023", "$80.000", "En Proceso"],
+  ["Forestal Leonera", "24-10-2023", "$50.000", "En Proceso","Dental", "24-10-2023", "$90.000", "En Proceso"],
+  ["Forestal Leonera", "24-10-2023", "$50.000", "En Proceso","Dental", "24-10-2023", "$11.000", "En Proceso"],
+  ["Forestal Leonera", "24-10-2023", "$50.000", "En Proceso","Dental", "24-10-2023", "$22.000", "En Proceso"],
+  ["Forestal Leonera", "24-10-2023", "$50.000", "En Proceso","Dental", "24-10-2023", "$33.000", "En Proceso"]
+];
 
 function App() {
+
+  const header = {
+    text: "Cristian A. Cortes Sarmiento",
+    styleText: {
+      fontFamily: "tahoma, san-serif",
+      fontSize: "32px",
+      fontWeight: "bold",
+      color: "red"
+    },
+    styleContainer: {
+      width: "100%",
+      height: "60px",
+      textAlign: "center",
+      backgroundColor: "yellow"
+    }
+  }
+
+  const column = {
+    styleText: {
+      fontFamily: "tahoma, san-serif",
+      fontSize: "12px",
+      fontWeight: "",
+      color: "#AABBCC"
+    },
+    styleContainer: {
+      width: "",
+      height: "15px",
+      textAlign: "center",
+      backgroundColor: "green"
+      
+    }
+  }
+
   return (
     <>
-        <Home/>
+    <Home/>
+      {/* <Container className='container'>
+        <PersonalizableTabla
+            header={header}
+            columnStyle={column}
+            columnHeader={["N° Consumo", "Fecha", "Hora", "Centro","Máquina", "Turno", "Jefe Turno", "Operador"]}
+            data={dataUltimosMov}
+            handlerEdit={()=>{alert("editar")}}
+            handlerInfo={()=>{alert("Info")}}
+            handlerDelete={()=>{alert("Delete")}}
+        />        
+      </Container> */}
+
     </>
   );
 }
--- src/components/PersonalizableTabla/index.tsx
+++ src/components/PersonalizableTabla/index.tsx
@@ -2,37 +2,37 @@ import {BsFillTrashFill, BsFillEyeFill, BsFillPencilFill} from "react-icons/bs"
 import { Table } from 'react-bootstrap';
 import './style.css';
 
-function Tabla(props: any) {
+function PersonalizableTabla(props: any) {
 
   return (
     <div className='container-fluid'>
-        <div  className="mb-4 fw-bold" >
-          {props.header}  
+        <div style={props.header.styleContainer}>
+          <label style={props.header.styleText}>{props.header.text}</label> 
         </div>
         <div className='table-responsive'>
-          <Table  className='sombra'>
-            <thead className='fontColumnHeader' >
-              <tr >
+          <Table bordered striped hover>
+            <thead >
+              <tr style={props.columnStyle.styleContainer} >
                 {
-                  props.columHeader.map((title: string, index: number) => <th className='text-center fw-bold ' key={index}>{title}</th>)
+                  props.columnHeader.map((title: string, index: number) => <th style={props.columnStyle.styleText} key={index}>{title}</th>)
                 }
                 {
                   <th key={9}>
-                    <div className='bg-success align-middle text-center rounded-circle '  style={{width: "30px", height: "30px"}}>
+                    <div className='bg-success align-middle text-center rounded-circle '  style={{height: "30px"}}>
                       <BsFillEyeFill title="Código" color={'white'} className='action mt-1 ' size={20}/>
                     </div>
                   </th>
                 }
                 {
                   <th key={10}  className='text-center' >
-                    <div className='bg-primary align-middle text-center rounded-circle '  style={{width: "30px", height: "30px"}}>
+                    <div className='bg-primary align-middle text-center rounded-circle '  style={{height: "30px"}}>
                       <BsFillPencilFill title="Código" color={'white'} className='action mt-1 ' size={20}/>
                     </div>               
                   </th>
                 }
                 {
                   <th key={11} className='text-center' >
-                    <div className='bg-danger align-middle text-center rounded-circle '  style={{width: "30px", height: "30px"}}>
+                    <div className='bg-danger align-middle text-center rounded-circle '  style={{height: "30px"}}>
                       <BsFillTrashFill title="Código" color={'white'} className='action mt-1 ' size={18}/>
                     </div>                     
                   </th>
@@ -49,7 +49,7 @@ function Tabla(props: any) {
                         {
                           <td key={9} className='text-center' >
                             <a href="#!"  onClick={()=>props.handlerEdit(index)}>
-                                <div className='bg-success align-middle text-center rounded-circle '  style={{width: "30px", height: "30px"}}>
+                                <div className='bg-success align-middle text-center rounded-circle '  style={{height: "30px"}}>
                                   <BsFillEyeFill title="Código" color={'white'} className='action mt-1 ' size={20}/>
                                 </div>
                             </a>
@@ -58,7 +58,7 @@ function Tabla(props: any) {
                         {
                           <td key={10} className='text-center' >
                             <a href="#!" onClick={()=>props.handlerInfo(index)}>
-                              <div className='bg-primary align-middle text-center rounded-circle '  style={{width: "30px", height: "30px"}}>
+                              <div className='bg-primary align-middle text-center rounded-circle '  style={{height: "30px"}}>
                                 <BsFillPencilFill title="Código" color={'white'} className='action mt-1 ' size={20}/>
                               </div>  
                             </a>
@@ -67,7 +67,7 @@ function Tabla(props: any) {
                         {
                           <td key={11} className='text-center' >
                             <a href="#!" onClick={()=> props.handlerDelete(index)}>
-                              <div className='bg-danger align-middle text-center rounded-circle '  style={{width: "30px", height: "30px"}}>
+                              <div className='bg-danger align-middle text-center rounded-circle '  style={{height: "30px"}}>
                                 <BsFillTrashFill title="Código" color={'white'} className='action mt-1 ' size={18}/>
                               </div>                               
                             </a>
@@ -83,4 +83,4 @@ function Tabla(props: any) {
   );
 }
 
-export default Tabla;
+export default PersonalizableTabla;
